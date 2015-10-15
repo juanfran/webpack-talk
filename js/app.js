10d8524c92508dd2b170fd94dc229d323f887138
Juanfran <juanfran.ag@gmail.com>
2015-10-15 21:09:24 +0200

    step-7

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ import styles from '../css/style.css';
 import * as test from './test';
 
 test.sayHi();
+
+console.log(styles.blue);
+
+// styles.blue -> _31AELaYi-Wo_AD9_W3JVGy
+// <div className={styles.blue}>Blue!</div>
